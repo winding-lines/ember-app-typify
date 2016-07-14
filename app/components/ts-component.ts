99bfa6f188f03b00f4fbbc9b37ea59904fac7631
Marius Seritan <git@winding-lines.com>
2016-07-14 08:13:18 -0700

    Add an example of local-types

--- app/components/ts-component.ts
+++ app/components/ts-component.ts
@@ -2,13 +2,40 @@
 import Ember from 'ember';
 import * as constants from '../lib/some-const';
 
-function compute() : {value: string} {
+// example of using a local-type definition
+import Hopscotch from 'npm:hopscotch';
+
+function compute(): { value: string } {
   if (constants.CHANGE) {
-    return { value: 'from ts'}
+    return { value: 'from ts' }
   } else {
-    return { value: 'won\'t happen'}
+    return { value: 'won\'t happen' }
   }
 }
+
+let seen = false;
+
+function teach() {
+  let tour = {
+    id: "introduction",
+    showCloseButton: "true",
+    steps: [
+      {
+        title: "Page Key Navigator",
+        content: "The Map contains the known page keys clustered in groups.",
+        target: document.querySelector("g.top"),
+        xOffset: 200,
+        arrowOffset: "center",
+        showCloseButton: true,
+        placement: "top"
+      }]
+  };
+   Hopscotch.listen('end', () => {
+        seen = true;
+      });
+  Hopscotch.startTour(tour);
+}
+
 export default Ember.Component.extend({
   lyft: Ember.inject.service(),
   cars: Ember.computed.readOnly('lyft.cars'),
---
+++ local-types/hopscotch/index.d.ts
@@ -0,0 +1,6 @@
+export default Hopscotch;
+
+declare namespace Hopscotch  {
+  function startTour(tour: any);
+  function listen(event: string, handler: () => any);
+}
